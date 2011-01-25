8bd8f3004276a145137cee9f00834e17a4f8344f
mausch <mauricioscheffer@gmail.com>
2011-01-25 01:42:29 -0300

    date formlet in vb

---
+++ Formlets.VB.Tests/FormletExtensions.vb
@@ -0,0 +1,14 @@
+﻿Imports Formlets.CSharp
+Imports System.Runtime.CompilerServices
+
+Module FormletExtensions
+    <Extension()> _
+    Public Function Validate(Of T)(ByVal formlet As Formlet(Of T), ByVal isValid As Func(Of T, Boolean), ByVal msg As Func(Of T, String)) As Formlet(Of T)
+        Return formlet.
+           Satisfies(isValid,
+                     Function(s, x) x.
+                       Append(<span class="errorMsg">'<%= msg(s) %>' is not a valid number</span>).
+                       WrapWith(<span class="error"/>))
+    End Function
+
+End Module
--- Formlets.VB.Tests/XmlIntegrationTests.vb
+++ Formlets.VB.Tests/XmlIntegrationTests.vb
@@ -3,21 +3,51 @@ Imports Formlets.CSharp
 Imports Xunit
 
 Public Class XmlIntegrationTests
+
     <Fact()> _
     Public Sub Test()
         Dim input = Formlet.Input()
-        Dim inputInt = Formlet.Input().
-                        Satisfies(Function(s) Regex.IsMatch(s, "[0-9]+"),
-                                   Function(s, x) x.
-                                       Append(<span class="errorMsg">'<%= s %>' is not a valid number</span>).
-                                       WrapWith(<span class="error"/>)).
-                        Lift(Function(a) Integer.Parse(a))
+        Dim inputInt =
+            Function(attr As IDictionary(Of String, String)) _
+                Formlet.Input(attr).Validate(Function(s) Regex.IsMatch(s, "[0-9]+"),
+                                      Function(s) String.Format("{0} is not a valid number")).
+                            Lift(Function(a) Integer.Parse(a))
+        Dim inputRange =
+            Function(min As Integer, max As Integer) _
+                inputInt(Nothing).Validate(Function(n) n <= max AndAlso n >= min,
+                                      Function(s) String.Format("Value must be between {0} and {1}", min, max))
+        Dim inputDate =
+            Function()
+                Dim isDate = Function(m As Integer, d As Integer, y As Integer)
+                                 Try
+                                     Dim k = New DateTime(y, m, d)
+                                     Return True
+                                 Catch ex As Exception
+                                     Return False
+                                 End Try
+                             End Function
+                Dim id = Guid.NewGuid.ToString()
+                Dim idn = Function(n As Integer) String.Format("d{0}{1}", id, n)
+                Dim values = Formlet.Yield(L.F(Function(month As Integer) _
+                              L.F(Function(day As Integer) _
+                              L.F(Function(year As Integer) tuple.Create(month, day, year))))).
+                    Ap(<label for=<%= idn(0) %>>Month: </label>).
+                    Ap(inputInt(New Dictionary(Of String, String) From {{"id", idn(0)}})).
+                    Ap(<br/>, <label for=<%= idn(1) %>>Day: </label>).
+                    Ap(inputInt(New Dictionary(Of String, String) From {{"id", idn(1)}})).
+                    Ap(<br/>, <label for=<%= idn(2) %>>Year: </label>).
+                    Ap(inputInt(New Dictionary(Of String, String) From {{"id", idn(2)}}))
+                Return values.Validate(Function(t) isDate(t.Item1, t.Item2, t.Item3),
+                                                          Function(s) "Invalid date").
+                            Lift(Function(t) New DateTime(t.Item3, t.Item1, t.Item2))
+            End Function
+
         Dim f =
             Formlet.Yield(L.F(Function(a As String) _
                           L.F(Function(b As Integer) Tuple.Create(a, b)))).
                 Ap(input).
                 Ap(<br/>, <br/>).
-                Ap(inputInt.WrapWith(<span class="something"/>)).
+                Ap(inputInt(Nothing).WrapWith(<span class="something"/>)).
                 Ap(<input type="submit" value="Send!"/>, <br/>)
         Console.WriteLine(f.Render())
         Dim result = f.Run(New Dictionary(Of String, String) From
