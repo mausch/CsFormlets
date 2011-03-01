2930d0aedcca816f51b243575187695ccccde3d4
mausch <mauricioscheffer@gmail.com>
2011-03-01 14:01:33 -0300

    refactored attribute conversion

--- Formlets.CSharp/FormElements.cs
+++ Formlets.CSharp/FormElements.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Attributes = System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>>;
+using Microsoft.FSharp.Core;
+using Microsoft.FSharp.Collections;
 
 namespace Formlets.CSharp {
     public class FormElements {
@@ -13,33 +16,33 @@ namespace Formlets.CSharp {
             e = new Formlets.FormElements(v);
         }
 
+        private FSharpOption<FSharpList<Tuple<string,string>>> ToAttr(Attributes attr) {
+            attr = attr ?? new Dictionary<string, string>();
+            return attr.ToTuples().ToFsList().ToOption();
+        }
+
         public Formlet<bool> Checkbox(bool value, bool required = false, Attributes attributes = null) {
-            attributes = attributes ?? new Dictionary<string, string>();
-            var f = e.Checkbox(value, required.ToOption(), attributes.ToTuples().ToFsList().ToOption());
+            var f = e.Checkbox(value, required.ToOption(), ToAttr(attributes));
             return new Formlet<bool>(f);
         }
 
         public Formlet<string> Textarea(string value = "", Attributes attributes = null) {
-            attributes = attributes ?? new Dictionary<string, string>();
-            var f = e.Textarea(value.ToOption(), attributes.ToTuples().ToFsList().ToOption());
+            var f = e.Textarea(value.ToOption(), ToAttr(attributes));
             return new Formlet<string>(f);
         }
 
         public Formlet<string> Text(string value = "", bool required = false, int? maxlength = null, string pattern = null, Attributes attributes = null) {
-            attributes = attributes ?? new Dictionary<string, string>();
-            var f = e.Text(value.ToOption(), attributes.ToTuples().ToFsList().ToOption(), required.ToOption(), maxlength.ToOption(), pattern.ToOption());
+            var f = e.Text(value.ToOption(), ToAttr(attributes), required.ToOption(), maxlength.ToOption(), pattern.ToOption());
             return new Formlet<string>(f);
         }
 
         public Formlet<double> Double(double? value = null, bool required = false, int? maxlength = null, double? min = null, double? max = null, Attributes attributes = null) {
-            attributes = attributes ?? new Dictionary<string, string>();
-            var f = e.Float(value.ToOption(), attributes.ToTuples().ToFsList().ToOption(), required.ToOption(), maxlength.ToOption(), min.ToOption(), max.ToOption());
+            var f = e.Float(value.ToOption(), ToAttr(attributes), required.ToOption(), maxlength.ToOption(), min.ToOption(), max.ToOption());
             return new Formlet<double>(f);
         }
 
         public Formlet<int> Int(int? value = null, bool required = false, int? maxlength = null, int? min = null, int? max = null, Attributes attributes = null) {
-            attributes = attributes ?? new Dictionary<string, string>();
-            var f = e.Int(value.ToOption(), attributes.ToTuples().ToFsList().ToOption(), required.ToOption(), maxlength.ToOption(), min.ToOption(), max.ToOption());
+            var f = e.Int(value.ToOption(), ToAttr(attributes), required.ToOption(), maxlength.ToOption(), min.ToOption(), max.ToOption());
             return new Formlet<int>(f);
         }
     }
