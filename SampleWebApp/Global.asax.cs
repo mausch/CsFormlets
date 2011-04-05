6df10a9ee448e9f1cd22ce4d79b40f4dcdf86760
mausch <mauricioscheffer@gmail.com>
2011-04-05 11:24:24 -0300

    signup controller is default

--- SampleWebApp/Global.asax.cs
+++ SampleWebApp/Global.asax.cs
@@ -24,7 +24,7 @@ namespace SampleWebApp
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Signup", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
