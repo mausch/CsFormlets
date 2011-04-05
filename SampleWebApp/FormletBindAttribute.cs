e67fbb6c9be5b766b63efdfd2e32230a5c4842c3
mausch <mauricioscheffer@gmail.com>
2011-04-05 12:56:08 -0300

    fixed handling of assigned action parameter

---
+++ SampleWebApp/FormletBindAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleWebApp {
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public class FormletBindAttribute: Attribute {}
+}
--- SampleWebApp/FormletPostAttribute.cs
+++ SampleWebApp/FormletPostAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using System.Reflection;
 using System.Collections.Generic;
@@ -49,7 +50,7 @@ namespace SampleWebApp {
             var methodName = formletMethodName ?? (filterContext.ActionDescriptor.ActionName + "Formlet");
             var method = type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
             if (method == null)
-                throw new Exception(string.Format("Formlet method '{0}' not found in '{1}'", formletMethodName, type));
+                throw new Exception(string.Format("Formlet method '{0}' not found in '{1}'", methodName, type));
             dynamic formlet = method.Invoke(filterContext.Controller, null);
             dynamic result = formlet.RunPost(filterContext.HttpContext.Request);
             if (result.Value == null) {
@@ -60,7 +61,17 @@ namespace SampleWebApp {
                     ViewData = new ViewDataDictionary(errorForm)
                 };
             } else {
-                filterContext.ActionParameters[formletMethodName] = result.Value.Value;
+                var value = result.Value.Value;
+                object option = result.Value;
+                var valueType = option.GetType().GetGenericArguments()[0];
+                var actionParams = filterContext.ActionDescriptor.GetParameters();
+                var boundParam = actionParams.FirstOrDefault(d => d.IsDefined(typeof(FormletBindAttribute), true));
+                if (boundParam == null)
+                    boundParam = actionParams.FirstOrDefault(d => d.ParameterType == valueType);
+                if (boundParam == null)
+                    throw new Exception(string.Format("Could not find any action parameter to bind formlet '{0}.{1}'. No action parameter of type '{2}' found and no action parameter was marked with [FormletBind]", type, methodName, valueType));
+
+                filterContext.ActionParameters[boundParam.ParameterName] = value;
             }
         }
     }
